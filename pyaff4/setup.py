4b5864d01a8952766aa99213565a69d08e213906
Mathieu Tarral <mathieu.tarral@f-secure.com>
2017-07-13 11:23:57 +0300

    update pyblake2 dependency to 0.9.3

--- pyaff4/setup.py
+++ pyaff4/setup.py
@@ -58,7 +58,7 @@ setup(
         "aff4-snappy == 0.5",
         "rdflib == 4.2.1",
         "intervaltree == 2.1.0",
-        "pyblake2 == 0.0.3",
+        "pyblake2 == 0.9.3",
         "expiringdict == 1.1.4"
     ],
     extras_require=dict(
