0b7912827b78f269aaf0bf85b73f86ffbccb9322
Michael Cohen <scudette@users.noreply.github.com>
2017-11-05 22:03:44 -0800

    Release 0.26post5. (#40)
    Incremented dependency on aff4-snappy.

--- pyaff4/setup.py
+++ pyaff4/setup.py
@@ -56,7 +56,7 @@ setup(
     package_dir={"pyaff4": "pyaff4"},
     install_requires=[
         "future",
-        "aff4-snappy == 0.5",
+        "aff4-snappy == 0.5.1",
         "rdflib[sparql] == 4.2.2",
         "intervaltree == 2.1.0",
         "pyblake2 == 0.9.3",
