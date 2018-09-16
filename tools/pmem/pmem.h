61f19320a105de6a49bc9ec858adab894ebe0e7e
Michael Cohen <scudette@users.noreply.github.com>
2018-09-17 09:16:49 +1000

    Fix mis-spelling of 32 bit driver. (#36)

--- tools/pmem/pmem.h
+++ tools/pmem/pmem.h
@@ -16,7 +16,7 @@ specific language governing permissions and limitations under the License.
 #ifndef TOOLS_PMEM_PMEM_H_
 #define TOOLS_PMEM_PMEM_H_
 
-#define PMEM_VERSION "3.0rc1";
+#define PMEM_VERSION "3.1rc3";
 
 #include <vector>
 #include "aff4/libaff4.h"
