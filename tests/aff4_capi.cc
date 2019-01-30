0d8122c607d9b188899a4db99aa93e0b9414f485
Michael Cohen <scudette@gmail.com>
2019-01-31 08:48:11 +1000

    bugfix for test.
    ASSERT_STREQ expects null terminated strings.

--- tests/aff4_capi.cc
+++ tests/aff4_capi.cc
@@ -48,8 +48,8 @@ TEST_F(AFF4CAPI, Sample1URN) {
     uint64_t size = AFF4_object_size(handle, nullptr);
     ASSERT_EQ(268435456, size);
 
-    char* buffer = (char*) malloc(32);
-    memset(buffer, 0, 32);
+    char* buffer = (char*) malloc(33);
+    memset(buffer, 0, 33);
     int read = AFF4_read(handle, 0, buffer, 32, nullptr);
     ASSERT_EQ(32, read);
     ASSERT_STREQ("\x33\xC0\x8E\xD0\xBC\x00\x7C\x8E\xC0\x8E\xD8\xBE\x00\x7C\xBF\x00\x06\xB9\x00\x02\xFC\xF3\xA4\x50\x68\x1C\x06\xCB\xFB\xB9\x04\x00", buffer);
@@ -68,8 +68,8 @@ TEST_F(AFF4CAPI, Sample2URN) {
     uint64_t size = AFF4_object_size(handle, nullptr);
     ASSERT_EQ(268435456, size);
 
-    char* buffer = (char*) malloc(32);
-    memset(buffer, 0, 32);
+    char* buffer = (char*) malloc(33);
+    memset(buffer, 0, 33);
 
     // Start
     int read = AFF4_read(handle, 0, buffer, 32, nullptr);
@@ -77,7 +77,7 @@ TEST_F(AFF4CAPI, Sample2URN) {
     ASSERT_STREQ("\x33\xC0\x8E\xD0\xBC\x00\x7C\x8E\xC0\x8E\xD8\xBE\x00\x7C\xBF\x00\x06\xB9\x00\x02\xFC\xF3\xA4\x50\x68\x1C\x06\xCB\xFB\xB9\x04\x00", buffer);
 
     // Unreadable
-    memset(buffer, 0, 32);
+    memset(buffer, 0, 33);
     read = AFF4_read(handle, 32326 * 512, buffer, 32, nullptr);
     ASSERT_EQ(32, read);
     ASSERT_STREQ("\x4D\xD9\x8B\x8C\xE2\x39\x44\x58\x6B\xA2\xA8\xDB\x04\x1C\x6D\x36\x81\x41\x36\x8B\x90\xA7\x16\xC2\x5E\x9A\x0C\xA6\xE6\xD9\x0B\x7E", buffer);
@@ -96,8 +96,8 @@ TEST_F(AFF4CAPI, Sample3URN) {
     uint64_t size = AFF4_object_size(handle, nullptr);
     ASSERT_EQ(268435456, size);
 
-    char* buffer = (char*) malloc(32);
-    memset(buffer, 0, 32);
+    char* buffer = (char*) malloc(33);
+    memset(buffer, 0, 33);
 
     // Start...
     int read = AFF4_read(handle, 0, buffer, 32, nullptr);
