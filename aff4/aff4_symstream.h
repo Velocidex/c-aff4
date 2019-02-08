867a1b58e4c56281d65e04dae2397d7e58528c30
Joel Uckelman <juckelman@strozfriedberg.co.uk>
2019-02-08 13:50:44 +0000

    AFF4SymbolicStream::Read() is now implemented in terms of AFF4SymbolicStream::ReadBuffer().

--- aff4/aff4_symstream.h
+++ aff4/aff4_symstream.h
@@ -24,7 +24,8 @@ class AFF4SymbolicStream: public AFF4Stream {
 
     virtual ~AFF4SymbolicStream();
 
-    std::string Read(size_t length);
+    std::string Read(size_t length) override;
+    AFF4Status ReadBuffer(char* data, size_t* length) override;
 
     // Override AFF4Object::Return();
     void Return();
