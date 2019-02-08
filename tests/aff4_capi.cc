d14dfbce1d800f7e1331766f19fb9445d48e74f3
Joel Uckelman <juckelman@strozfriedberg.co.uk>
2019-02-08 16:05:05 +0000

    Cleanup.

--- tests/aff4_capi.cc
+++ tests/aff4_capi.cc
@@ -13,9 +13,9 @@
   specific language governing permissions and limitations under the License.
 */
 #include <gtest/gtest.h>
-#include "aff4/libaff4-c.h"
-#include <unistd.h>
 #include <glog/logging.h>
+
+#include "aff4/libaff4-c.h"
 
 namespace aff4 {
 
@@ -48,13 +48,12 @@ TEST_F(AFF4CAPI, Sample1URN) {
     uint64_t size = AFF4_object_size(handle, nullptr);
     ASSERT_EQ(268435456, size);
 
-    char* buffer = (char*) malloc(33);
+    char buffer[33];
     memset(buffer, 0, 33);
     ssize_t read = AFF4_read(handle, 0, buffer, 32, nullptr);
     ASSERT_EQ(32, read);
     ASSERT_STREQ("\x33\xC0\x8E\xD0\xBC\x00\x7C\x8E\xC0\x8E\xD8\xBE\x00\x7C\xBF\x00\x06\xB9\x00\x02\xFC\xF3\xA4\x50\x68\x1C\x06\xCB\xFB\xB9\x04\x00", buffer);
 
-    free(buffer);
     AFF4_close(handle, nullptr);
 }
 
@@ -68,7 +67,7 @@ TEST_F(AFF4CAPI, Sample2URN) {
     uint64_t size = AFF4_object_size(handle, nullptr);
     ASSERT_EQ(268435456, size);
 
-    char* buffer = (char*) malloc(33);
+    char buffer[33];
     memset(buffer, 0, 33);
 
     // Start
@@ -82,7 +81,6 @@ TEST_F(AFF4CAPI, Sample2URN) {
     ASSERT_EQ(32, read);
     ASSERT_STREQ("\x4D\xD9\x8B\x8C\xE2\x39\x44\x58\x6B\xA2\xA8\xDB\x04\x1C\x6D\x36\x81\x41\x36\x8B\x90\xA7\x16\xC2\x5E\x9A\x0C\xA6\xE6\xD9\x0B\x7E", buffer);
 
-    free(buffer);
     AFF4_close(handle, nullptr);
 }
 
@@ -96,7 +94,7 @@ TEST_F(AFF4CAPI, Sample3URN) {
     uint64_t size = AFF4_object_size(handle, nullptr);
     ASSERT_EQ(268435456, size);
 
-    char* buffer = (char*) malloc(33);
+    char buffer[33];
     memset(buffer, 0, 33);
 
     // Start...
@@ -110,7 +108,6 @@ TEST_F(AFF4CAPI, Sample3URN) {
     ASSERT_EQ(32, read);
     ASSERT_STREQ("\x55\x4E\x52\x45\x41\x44\x41\x42\x4C\x45\x44\x41\x54\x41\x55\x4E\x52\x45\x41\x44\x41\x42\x4C\x45\x44\x41\x54\x41\x55\x4E\x52\x45", buffer);
 
-    free(buffer);
     AFF4_close(handle, nullptr);
 }
 
