0416f9cbdd52127248cc96046fc8bb715614157e
Joel Uckelman <juckelman@strozfriedberg.co.uk>
2019-02-08 13:33:58 +0000

    Cleanup.

--- aff4/aff4_io.h
+++ aff4/aff4_io.h
@@ -160,7 +160,7 @@ class AFF4Stream: public AFF4Object {
     virtual AFF4Status Seek(aff4_off_t offset, int whence);
     virtual std::string Read(size_t length);
 
-    virtual AFF4Status ReadBuffer(char *data, size_t *length);
+    virtual AFF4Status ReadBuffer(char* data, size_t* length);
 
     virtual AFF4Status Write(const char* data, size_t length);
     virtual aff4_off_t Tell();
